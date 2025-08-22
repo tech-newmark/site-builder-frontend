b32b4b10a64ea6aa7d2ac04fb525e6b43673e693
Oksana Tarasova <oksanchik_90@mail.ru>
2025-08-22 23:03:29 +0300

    Верстка разных вариантов шапок

--- src/pug/layouts/header/script.js
+++ src/pug/layouts/header/script.js
@@ -5,17 +5,34 @@ const mobileMenuContent = document.querySelector(
 );
 const closeButton = document.querySelector(".header__mobile-menu-close-button");
 
-burgerButton.addEventListener("click", () => {
-  mobileMenu.style.zIndex = "40";
-  mobileMenu.style.opacity = "1";
-  mobileMenuContent.style.transform = "translateX(0)";
-});
+if (burgerButton) {
+  burgerButton.addEventListener("click", () => {
+    mobileMenu.style.zIndex = "40";
+    mobileMenu.style.opacity = "1";
+    mobileMenuContent.style.transform = "translateX(0)";
+  });
+}
 
-closeButton.addEventListener("click", () => {
-  console.log("click");
+const closeMobileMenu = () => {
   mobileMenuContent.style.transform = "translateX(-100vw)";
   setTimeout(() => {
     mobileMenu.style.opacity = "0";
     mobileMenu.style.zIndex = "-1";
   }, 300);
+};
+
+if (closeButton) {
+  closeButton.addEventListener("click", () => closeMobileMenu());
+}
+
+document.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
+    closeMobileMenu();
+  }
+});
+
+document.addEventListener("click", (evt) => {
+  if (evt.target === mobileMenu) {
+    closeMobileMenu();
+  }
 });
