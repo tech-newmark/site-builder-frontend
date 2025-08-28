b19bc8fd6255317028d5197ef5ff5715caba01e4
Oksana Tarasova <oksanchik_90@mail.ru>
2025-08-28 21:05:06 +0300

    Рефакторинг кода

--- src/pug/layouts/header/script.js
+++ src/pug/layouts/header/script.js
@@ -1,42 +1,42 @@
-const burgerButton = document.querySelector(".header__action-button--burger");
-const mobileMenu = document.querySelector(".header__mobile-menu");
-const mobileMenuContent = document.querySelector(
-  ".header__mobile-menu-content"
-);
-const closeButton = document.querySelector(".header__action-button--close");
+// const burgerButton = document.querySelector(".header__action-button--burger");
+// const mobileMenu = document.querySelector(".header__mobile-menu");
+// const mobileMenuContent = document.querySelector(
+//   ".header__mobile-menu-content"
+// );
+// const closeButton = document.querySelector(".header__action-button--close");
 
-const openMobileMenu = () => {
-  mobileMenu.style.display = "block";
-  mobileMenu.style.opacity = "1";
-  mobileMenu.style.zIndex = "40";
-  mobileMenuContent.style.transform = "translateX(0)";
-};
+// const openMobileMenu = () => {
+//   mobileMenu.style.display = "block";
+//   mobileMenu.style.opacity = "1";
+//   mobileMenu.style.zIndex = "40";
+//   mobileMenuContent.style.transform = "translateX(0)";
+// };
 
-const closeMobileMenu = () => {
-  mobileMenuContent.style.transform = "translateX(-100vw)";
-  setTimeout(() => {
-    mobileMenu.style.display = "none";
-    mobileMenu.style.opacity = "0";
-    mobileMenu.style.zIndex = "-1";
-  }, 300);
-};
+// const closeMobileMenu = () => {
+//   mobileMenuContent.style.transform = "translateX(-100vw)";
+//   setTimeout(() => {
+//     mobileMenu.style.display = "none";
+//     mobileMenu.style.opacity = "0";
+//     mobileMenu.style.zIndex = "-1";
+//   }, 300);
+// };
 
-if (burgerButton) {
-  burgerButton.addEventListener("click", () => openMobileMenu());
-}
+// if (burgerButton) {
+//   burgerButton.addEventListener("click", () => openMobileMenu());
+// }
 
-if (closeButton) {
-  closeButton.addEventListener("click", () => closeMobileMenu());
-}
+// if (closeButton) {
+//   closeButton.addEventListener("click", () => closeMobileMenu());
+// }
 
-document.addEventListener("keydown", (evt) => {
-  if (evt.key === "Escape") {
-    closeMobileMenu();
-  }
-});
+// document.addEventListener("keydown", (evt) => {
+//   if (evt.key === "Escape") {
+//     closeMobileMenu();
+//   }
+// });
 
-document.addEventListener("click", (evt) => {
-  if (evt.target === mobileMenu) {
-    closeMobileMenu();
-  }
-});
+// document.addEventListener("click", (evt) => {
+//   if (evt.target === mobileMenu) {
+//     closeMobileMenu();
+//   }
+// });
